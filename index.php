9422ac00849ac43646f3c29292b02a3aaac2cb2d
pantera17 <josenaranjo2004.jn@gmail.com>
2021-12-18 09:00:49 -0500

    Sintaxis basica y variables

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+// echo "hola, mundo";
+
+$nombre = "Jose";
+$apellido = "Pantera";
+
+// echo "No, no. Yo me llamo " . $nombre . " " . $apellido. "\n";
+
+echo "No, no. Yo me llamo $nombre $apellido \n";  //es mas facil de leer que el anterior solo funciona con comillas dobles
+
+ echo "El resultado de 4 x 5 es ". (4 * 5);
+
+$personas = [
+    "Carlos" => 22,
+    "Mr, Michi" => 15,
+    "Juan" => 65
+];
+
+var_dump($personas); //brinda mayor informacion
+print_r($personas); //mas facil de leer 
---
+++ variables.php
@@ -0,0 +1,18 @@
+<?php
+
+$numero_1 = 8;
+$numero_2 = 7;
+
+define("NUMERO_PI", 3.14);
+define("NUMERO_PI", 14);
+
+/* 
+Constantes se definen con define el nombre por 
+buenas practicas va en mayusculas
+*/
+
+echo $numero_1 + $numero_2;
+
+echo "\n";
+
+echo NUMERO_PI;
